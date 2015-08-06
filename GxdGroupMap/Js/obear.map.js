e18f42523661dbfabb2551e4eeffe607c40eb6a8
daywrite@yeah.net <daywrite@yeah.net>
2015-08-06 12:57:17 +0800

    地图和标注点展示

--- GxdGroupMap/Js/obear.map.js
+++ GxdGroupMap/Js/obear.map.js
@@ -1,6 +1,41 @@
 ﻿define(["obear"], function (obear) {
+    var BaiduMap = function (ele, opt) {
+        this.element = ele,
+        this.defaults = {
+            lng: 116.404,
+            lat: 39.915,
+            level: 15,
+            scroll: true
+        };
+        this.options = $.extend(this.defaults, opt || {});
+    };
+    BaiduMap.prototype = {
+        create: function () {
+            var map = new BMap.Map(this.element);
+            var point = new BMap.Point(this.options.lng, this.options.lat);
+            map.centerAndZoom(point, this.options.level);
+            map.enableDragging();
+            if (this.options.scroll) {
+                map.enableScrollWheelZoom();
+            }
+            map.enableContinuousZoom();
+            map.disableDoubleClickZoom();
 
-    return {
-        O: "321"
+            return map;
+        }
+    };
+
+    //创建一个百度地图
+    obear.CreateBaiduMap = function (element, options) {
+        var baiduMap = new BaiduMap(element, options);
+        return baiduMap.create();
+    };
+    obear.AddMarker = function (map, x, y) {
+            map.clearOverlays();
+            var new_point = new BMap.Point(x, y);
+            var marker = new BMap.Marker(new_point);
+            map.addOverlay(marker);
+
+            return new_point;
     };
 });
--- GxdGroupMap/Js/require.map.index.main.js
+++ GxdGroupMap/Js/require.map.index.main.js
@@ -2,11 +2,14 @@
     baseUrl: '../Js',
     paths: {
         jquery: 'jquery-2.1.4',
-        obear:'obear',
+        obear: 'obear',
         obmap: 'obear.map'
     }
 });
 
 require(['obmap'], function (obmap) {
-    alert(obmap.O);
+    //创建一个地图
+    var map = $.obear.CreateBaiduMap($("#mapCanvas")[0]);
+    //添加标注
+    $.obear.AddMarker(map,116.404, 39.915);
 });
