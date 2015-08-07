74c3ad0dbf1f9c2b7f75208cf55cca64660bb1a0
传熙 孙 <bitsunchuanxi@163.com>
2015-08-07 14:56:16 +0800

    自定义图标 地图控件

--- GxdGroupMap/Js/obear.map.js
+++ GxdGroupMap/Js/obear.map.js
@@ -13,6 +13,9 @@
         create: function () {
             var map = new BMap.Map(this.element);
             var point = new BMap.Point(this.options.lng, this.options.lat);
+            var top_left_control = new BMap.ScaleControl({ anchor: BMAP_ANCHOR_BOTTOM_LEFT });
+            var top_left_navigation = new BMap.NavigationControl({ anchor: BMAP_ANCHOR_BOTTOM_LEFT });  //左上角，添加默认缩放平移控件
+            var mapType2 = new BMap.MapTypeControl({ anchor: BMAP_ANCHOR_TOP_LEFT });
             map.centerAndZoom(point, this.options.level);
             map.enableDragging();
             if (this.options.scroll) {
@@ -20,7 +23,14 @@
             }
             map.enableContinuousZoom();
             map.disableDoubleClickZoom();
-
+            map.addControl(top_left_control);
+            map.addControl(top_left_navigation);
+            map.addControl(mapType2);
+            map.setCurrentCity("北京");
+            //function showinfo(e) {
+            //    alert(e.point.lng + "," + e.point.lat);
+            //}
+            //map.addEventListener("click", showinfo);
             return map;
         }
     };
@@ -35,7 +45,20 @@
             var new_point = new BMap.Point(x, y);
             var marker = new BMap.Marker(new_point);
             map.addOverlay(marker);
-
             return new_point;
     };
+    obear.AddPicture = function (map,picture,x,y)
+    {
+        map.clearOverlays();
+        var sAddress = picture;
+        var point = new BMap.Point(x, y);
+        var sIcon = new BMap.Icon(sAddress, new BMap.Size(300, 157));
+        var marker2 = new BMap.Marker(point, { icon: sIcon });  // 创建标注
+        marker2.enableDragging();  
+        map.addOverlay(marker2); //放置图标
+    }
+    //obear.Search=function(map,array)
+    //{
+    //    var 
+    //}
 });
--- GxdGroupMap/Js/require.map.index.main.js
+++ GxdGroupMap/Js/require.map.index.main.js
@@ -11,5 +11,9 @@ require(['obmap'], function (obmap) {
     //创建一个地图
     var map = $.obear.CreateBaiduMap($("#mapCanvas")[0]);
     //添加标注
-    $.obear.AddMarker(map,116.404, 39.915);
+    $.obear.AddMarker(map, 116.404, 39.915);
+    //添加自定义图标
+    $('#picturebutton').click(function () {
+        $.obear.AddPicture(map, "http://developer.baidu.com/map/jsdemo/img/fox.gif", 116.404, 39.915);
+    });
 });
