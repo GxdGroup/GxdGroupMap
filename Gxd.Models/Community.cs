01f73f76eb865f7161d5ee295503c2cf5562749e
daywrite@yeah.net <daywrite@yeah.net>
2015-08-10 09:41:30 +0800

    Dapper获取数据

---
+++ Gxd.Models/Community.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gxd.Models
+{
+    public class Community
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Lng { get; set; }
+        public double Lat { get; set; }
+    }
+}
--- GxdGroupMap/Controllers/MapController.cs
+++ GxdGroupMap/Controllers/MapController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Gxd.Domain;
+using Gxd.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,7 +13,13 @@ namespace GxdGroupMap.Controllers
         // GET: Map
         public ActionResult Index()
         {
+            int Id = 1;
+            string commandText = @"SELECT * FROM b_community where Id = @Id";
+            IEnumerable<Community> comlists = DbHelper.Query<Community>(commandText, new { Id = Id }, null);
+            List<Community> _comlists = comlists.ToList<Community>();
             return View();
         }
+
+
     }
 }
