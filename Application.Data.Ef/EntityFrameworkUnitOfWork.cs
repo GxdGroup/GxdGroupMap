b4bcbd2a4b2a1edce9d2647baef291359fa262fd
daywrite@yeah.net <daywrite@yeah.net>
2015-08-11 11:42:29 +0800

    未完成的EF代码

---
+++ Application.Data.Ef/EntityFrameworkUnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Data.Ef
+{
+    /// <summary>
+    /// 应用程序工作单元
+    /// </summary>
+    public class ApplicationUnitOfWork 
+    {      
+    }
+}
+
