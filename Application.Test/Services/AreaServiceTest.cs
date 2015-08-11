d49876b4d073b51dbe6645d7a98540fabf68c7a2
daywrite@yeah.net <daywrite@yeah.net>
2015-08-11 14:08:35 +0800

    单元测试依赖注入失败，通过new的方式单元测试

---
+++ Application.Test/Services/AreaServiceTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Application.Service;
+using Gxd.ApplicationServices;
+using Application.Domain;
+using Application.Data.Dapper;
+using Application.Data.Dapper.Repositories;
+
+namespace Application.Test.Services
+{
+    [TestClass]
+    public class AreaServiceTest
+    {
+        IAreaContract _areaContract;
+        /// <summary>
+        /// 测试初始化
+        /// </summary>
+        [TestInitialize]
+        public void TestInit()
+        {
+            IApplicationUnitOfWork unitOfWork = new DapperUnitOfWork();
+            ICommunityRepository communityReposity = new CommunityRepository(unitOfWork);
+             _areaContract=new AreaService(unitOfWork, communityReposity);            
+        }
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int Id = 1;
+            string commandText = @"SELECT * FROM b_community where Id = @Id";
+            var community = _areaContract.Communities(commandText, new { Id = Id });
+            Assert.AreEqual(1, community.Count);
+        }
+    }
+}
