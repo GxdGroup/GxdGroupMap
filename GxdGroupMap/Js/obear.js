784bcc73c670f600c90ab64c6c43aad95a59209e
传熙 孙 <bitsunchuanxi@163.com>
2015-08-20 21:03:02 +0800

    合并测试123

--- GxdGroupMap/Js/obear.js
+++ GxdGroupMap/Js/obear.js
@@ -1,6 +1,6 @@
 ﻿define(["jquery"], function ($) {
     $.obear = $.obear || { version: 1.0 };
-    $.obear.name = "123";
+    $.obear.name = "1234";
     $.obear.password = "12";
     return $.obear;
 });
