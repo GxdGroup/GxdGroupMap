59572470e6e0b984b684d1644e23121a57ae3adf
daywrite@yeah.net <daywrite@yeah.net>
2015-08-20 20:52:00 +0800

    测试合并

--- GxdGroupMap/Js/obear.js
+++ GxdGroupMap/Js/obear.js
@@ -1,5 +1,6 @@
 ﻿define(["jquery"], function ($) {
     $.obear = $.obear || { version: 1.0 };
-    $.obear.name = "123";
+    $.obear.name = "000";
+    $.obear.email = "321";
     return $.obear;
 });
